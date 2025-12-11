e58f92c7412817d66657f1d6ae3a2f3bcd8343d0
SpheraXX <binbo242@gmail.com>
2025-12-11 15:35:53 +0700

    Testing

---
+++ include/RecurringConfiguration.h
@@ -0,0 +1,11 @@
+#ifndef RECURRINGCONFIGURATION_H
+#define RECURRINGCONFIGURATION_H
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<iomanip>
+#include<math.h>
+#include<cmath>
+
+#endif
